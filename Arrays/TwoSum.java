441d80a45d633bf0dedb3ae8906802086b5b4431
anees215 <21ec267@students.stjosephs.ac.in>
2025-08-04 23:44:31 +0530

    first commit

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,9 @@
+// Problem: Find two numbers in the array that add up to a specific target.
+
+package Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println("This is Two Sum solution in java");
+    }
+}
