8161d3ca92fa670ec657db244f56937606ff4a72
anees215 <21ec267@students.stjosephs.ac.in>
2025-08-06 23:14:37 +0530

    File added

---
+++ Strings/LongestPalindromicSubstring.java
@@ -0,0 +1,45 @@
+package Strings;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String str = "babcd";
+        String result = finLongestPalindromicSubstring(str);
+        System.out.println(result);
+    }
+    
+    public static String finLongestPalindromicSubstring(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        int maxLen = 1;
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxLen; j <= s.length(); j++) {
+                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                    maxLen = j - i;
+                    maxStr = s.substring(i, j);
+                }
+            }
+        }
+
+        return maxStr;
+    }
+
+    private static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
